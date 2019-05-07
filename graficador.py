dfbdd77801f6df2d9a11871f872f91cdef753a0d
SantiagoBeltran <46940845+SantiagoBeltran@users.noreply.github.com>
2019-05-06 19:41:39 -0500

    Graficador completo

--- graficador.py
+++ graficador.py
@@ -1,18 +1,45 @@
-import numpy as np
+from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
+
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+import numpy as np
+
+fig = plt.figure(figsize=(12,5))
+ax = fig.add_subplot(1, 2, 1, projection='3d')
+#ax = fig.gca(projection='3d')
 
 data= np.loadtxt("intento.dat")
-x=np.arange(0,100,1)
-y=np.arange(0,100,1)
+x=np.arange(0,101,1)
+y=np.arange(0,301,1)
+x1=np.arange(0,101,1)/100
+y1=np.arange(0,301,1)*6/300
 
 def f(x,y):
     return data[x,y]
 
 X,Y=np.meshgrid(x,y)
+X1,Y1=np.meshgrid(x1,y1)
 
-fig = plt.figure()
-ax = Axes3D(fig)
-ax.plot_wireframe(X,Y,f(X,Y),color="r")
+# Plot the surface.
+surf = ax.plot_surface(Y1, X1, f(Y,X), cmap=cm.coolwarm,
+                       linewidth=0, antialiased=True)
+
+# Customize the z axis.
+#ax.set_zlim(-1.01, 1.01)
+#ax.zaxis.set_major_locator(LinearLocator(10))
+#ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+ax.set_xlabel("Tiempo [segundos]")
+ax.set_ylabel("Posicion [metros]")
+
+
+# Add a color bar which maps values to colors.
+fig.colorbar(surf, shrink=0.5, aspect=10, label="Desplazamiento [metros]", ticks=[-0.75,-0.5,-0.25,0,0.25,0.5,0.75])
+
+ax1 = fig.add_subplot(1, 2, 2)
+ax1.plot(x1,data[0], label="tiempo inicial")
+ax1.plot(x1,data[-1], label="tiempo final")
+ax1.legend()
+ax1.set_xlabel("Posicion [metros]")
 plt.show()
-plt.savefig("laplace.png")
+plt.savefig("onda.png")
